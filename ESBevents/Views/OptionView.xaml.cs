a4bfd48c5dac0191bb3165bb8528900c5ffece03
jplatenb <jplatenb@csc.com>
2017-05-19 13:22:20 +0200

    Aanpassingen in Rijswijk

--- ESBevents/Views/OptionView.xaml.cs
+++ ESBevents/Views/OptionView.xaml.cs
@@ -16,30 +16,30 @@ namespace ESBevents
 		{
 			var ik = sender as Button;
 
-			Application.Current.Properties["ipALL"] = ipALL.Text;
-			Application.Current.Properties["ipALLtst"] = ipALLtst.Text;
-			Application.Current.Properties["portEventsALL"] = portEventsALL.Text;
-			Application.Current.Properties["portProcessALL"] = portProcessALL.Text;
+			//Application.Current.Properties["ipALL"] = ipALL.Text;
+			//Application.Current.Properties["ipALLtst"] = ipALLtst.Text;
+			//Application.Current.Properties["portEventsALL"] = portEventsALL.Text;
+			//Application.Current.Properties["portProcessALL"] = portProcessALL.Text;
 
-			Application.Current.Properties["ipDBZ"] = ipDBZ.Text;
-			Application.Current.Properties["ipDBZtst"] = ipDBZtst.Text;
-			Application.Current.Properties["portEventsDBZ"] = portEventsDBZ.Text;
-			Application.Current.Properties["portProcessDBZ"] = portProcessDBZ.Text;
+			//Application.Current.Properties["ipDBZ"] = ipDBZ.Text;
+			//Application.Current.Properties["ipDBZtst"] = ipDBZtst.Text;
+			//Application.Current.Properties["portEventsDBZ"] = portEventsDBZ.Text;
+			//Application.Current.Properties["portProcessDBZ"] = portProcessDBZ.Text;
 
-			Application.Current.Properties["ipSPZ"] = ipSPZ.Text;
-			Application.Current.Properties["ipSPZtst"] = ipSPZtst.Text;
-			Application.Current.Properties["portEventsSPZ"] = portEventsSPZ.Text;
-			Application.Current.Properties["portProcessSPZ"] = portProcessSPZ.Text;
+			//Application.Current.Properties["ipSPZ"] = ipSPZ.Text;
+			//Application.Current.Properties["ipSPZtst"] = ipSPZtst.Text;
+			//Application.Current.Properties["portEventsSPZ"] = portEventsSPZ.Text;
+			//Application.Current.Properties["portProcessSPZ"] = portProcessSPZ.Text;
 
-			Application.Current.Properties["ipSHL"] = ipSHL.Text;
-			Application.Current.Properties["ipSHLtst"] = ipSHLtst.Text;
-			Application.Current.Properties["portEventsSHL"] = portEventsSHL.Text;
-			Application.Current.Properties["portProcessSHL"] = portProcessSHL.Text;
+			//Application.Current.Properties["ipSHL"] = ipSHL.Text;
+			//Application.Current.Properties["ipSHLtst"] = ipSHLtst.Text;
+			//Application.Current.Properties["portEventsSHL"] = portEventsSHL.Text;
+			//Application.Current.Properties["portProcessSHL"] = portProcessSHL.Text;
 
-			Application.Current.Properties["ipZGB"] = ipZGB.Text;
-			Application.Current.Properties["ipZGBtst"] = ipZGBtst.Text;
-			Application.Current.Properties["portEventsZGB"] = portEventsZGB.Text;
-			Application.Current.Properties["portProcessZGB"] = portProcessZGB.Text;
+			//Application.Current.Properties["ipZGB"] = ipZGB.Text;
+			//Application.Current.Properties["ipZGBtst"] = ipZGBtst.Text;
+			//Application.Current.Properties["portEventsZGB"] = portEventsZGB.Text;
+			//Application.Current.Properties["portProcessZGB"] = portProcessZGB.Text;
 
 		}
 	}
