98d53edd394beb70f4d828d577a97630c3ed75ec
Joost Platenburg <jplatenb@dxc.com>
2017-05-20 13:05:28 +0200

    Option page fixes

--- ESBevents/Views/OptionView.xaml.cs
+++ ESBevents/Views/OptionView.xaml.cs
@@ -10,36 +10,100 @@ namespace ESBevents
 		public OptionView()
 		{
 			InitializeComponent();
+
+			SetTextValue("ipALLO", ipALLO);
+			SetTextValue("ipALLT", ipALLT);
+			SetTextValue("ipALLA", ipALLA);
+			SetTextValue("ipALLP", ipALLP);
+
+			SetTextValue("ipDBZO", ipDBZO);
+			SetTextValue("ipDBZT", ipDBZT);
+			SetTextValue("ipDBZA", ipDBZA);
+			SetTextValue("ipDBZP", ipDBZP);
+
+			SetTextValue("ipSHLO", ipSHLO);
+			SetTextValue("ipSHLT", ipSHLT);
+			SetTextValue("ipSHLA", ipSHLA);
+			SetTextValue("ipSHLP", ipSHLP);
+
+			SetTextValue("ipSPZO", ipSPZO);
+			SetTextValue("ipSPZT", ipSPZT);
+			SetTextValue("ipSPZA", ipSPZA);
+			SetTextValue("ipSPZP", ipSPZP);
+
+			SetTextValue("ipZGBO", ipZGBO);
+			SetTextValue("ipZGBT", ipZGBT);
+			SetTextValue("ipZGBA", ipZGBA);
+			SetTextValue("ipZGBP", ipZGBP);
+
+			SetTextValue("portEventsALL", portEventsALL);
+			SetTextValue("portProcessALL", portProcessALL);
+			SetTextValue("portEventsDBZ", portEventsDBZ);
+			SetTextValue("portProcessDBZ", portProcessDBZ);
+			SetTextValue("portEventsSHL", portEventsSHL);
+			SetTextValue("portProcessSHL", portProcessSHL);
+			SetTextValue("portEventsSPZ", portEventsSPZ);
+			SetTextValue("portProcessSPZ", portProcessSPZ);
+			SetTextValue("portEventsZGB", portEventsZGB);
+			SetTextValue("portProcessZGB", portProcessZGB);
 		}
 
 		void SaveOptions(object sender, EventArgs e)
 		{
 			var ik = sender as Button;
 
-			//Application.Current.Properties["ipALL"] = ipALL.Text;
-			//Application.Current.Properties["ipALLtst"] = ipALLtst.Text;
-			//Application.Current.Properties["portEventsALL"] = portEventsALL.Text;
-			//Application.Current.Properties["portProcessALL"] = portProcessALL.Text;
+			SaveTextValue("ipALLO", ipALLO);
+			SaveTextValue("ipALLT", ipALLT);
+			SaveTextValue("ipALLA", ipALLA);
+			SaveTextValue("ipALLP", ipALLP);
 
-			//Application.Current.Properties["ipDBZ"] = ipDBZ.Text;
-			//Application.Current.Properties["ipDBZtst"] = ipDBZtst.Text;
-			//Application.Current.Properties["portEventsDBZ"] = portEventsDBZ.Text;
-			//Application.Current.Properties["portProcessDBZ"] = portProcessDBZ.Text;
+			SaveTextValue("ipDBZO", ipDBZO);
+			SaveTextValue("ipDBZT", ipDBZT);
+			SaveTextValue("ipDBZA", ipDBZA);
+			SaveTextValue("ipDBZP", ipDBZP);
 
-			//Application.Current.Properties["ipSPZ"] = ipSPZ.Text;
-			//Application.Current.Properties["ipSPZtst"] = ipSPZtst.Text;
-			//Application.Current.Properties["portEventsSPZ"] = portEventsSPZ.Text;
-			//Application.Current.Properties["portProcessSPZ"] = portProcessSPZ.Text;
+			SaveTextValue("ipSHLO", ipSHLO);
+			SaveTextValue("ipSHLT", ipSHLT);
+			SaveTextValue("ipSHLA", ipSHLA);
+			SaveTextValue("ipSHLP", ipSHLP);
 
-			//Application.Current.Properties["ipSHL"] = ipSHL.Text;
-			//Application.Current.Properties["ipSHLtst"] = ipSHLtst.Text;
-			//Application.Current.Properties["portEventsSHL"] = portEventsSHL.Text;
-			//Application.Current.Properties["portProcessSHL"] = portProcessSHL.Text;
+			SaveTextValue("ipSPZO", ipSPZO);
+			SaveTextValue("ipSPZT", ipSPZT);
+			SaveTextValue("ipSPZA", ipSPZA);
+			SaveTextValue("ipSPZP", ipSPZP);
 
-			//Application.Current.Properties["ipZGB"] = ipZGB.Text;
-			//Application.Current.Properties["ipZGBtst"] = ipZGBtst.Text;
-			//Application.Current.Properties["portEventsZGB"] = portEventsZGB.Text;
-			//Application.Current.Properties["portProcessZGB"] = portProcessZGB.Text;
+			SaveTextValue("ipZGBO", ipZGBO);
+			SaveTextValue("ipZGBT", ipZGBT);
+			SaveTextValue("ipZGBA", ipZGBA);
+			SaveTextValue("ipZGBP", ipZGBP);
+
+            SaveTextValue("portEventsALL", portEventsALL);
+            SaveTextValue("portProcessALL", portProcessALL);
+			SaveTextValue("portEventsDBZ", portEventsDBZ);
+			SaveTextValue("portProcessDBZ", portProcessDBZ);
+			SaveTextValue("portEventsSHL", portEventsSHL);
+			SaveTextValue("portProcessSHL", portProcessSHL);
+			SaveTextValue("portEventsSPZ", portEventsSPZ);
+			SaveTextValue("portProcessSPZ", portProcessSPZ);
+			SaveTextValue("portEventsZGB", portEventsZGB);
+			SaveTextValue("portProcessZGB", portProcessZGB);
+
+            Navigation.PopModalAsync(true);
+		}
+
+        void SetTextValue(string Key, Entry obj) {
+			if (Application.Current.Properties.ContainsKey(Key))
+			{
+				obj.Text = Application.Current.Properties[Key] as string;
+			}
+		}
+
+		void SaveTextValue(string Key, Entry obj)
+		{
+			if (obj.Text != null)
+			{
+				Application.Current.Properties[Key] = obj.Text.Replace(",", ".");
+			}
 
 		}
 	}
